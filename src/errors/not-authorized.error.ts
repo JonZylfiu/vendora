764701786747976009906078007dc3a84b94e92d
JonZylfiu <jonzylfiu5@gmail.com>
2026-05-27 20:50:56 +0200

    Created NotAuthorizedError for throwing errors regarding with authorization

---
+++ src/errors/not-authorized.error.ts
@@ -0,0 +1,19 @@
+import { CustomError } from "./custom.error.js";
+
+
+export default class NotAuthorizedError extends CustomError {
+    private static readonly _statusCode = 403;
+    private readonly _code: number;
+
+    constructor(params?: {message?: string, code?: number}) {
+        const { message, code } = params || {};
+
+        super(message || "You're not authorized!");
+
+        this._code = code || NotAuthorizedError._statusCode; 
+    }
+
+    get statusCode() {
+        return this._code;
+    }
+}
