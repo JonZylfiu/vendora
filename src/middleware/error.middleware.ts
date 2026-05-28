7d6d8566d0810818c133e9ed4b770f9501982d78
JonZylfiu <jonzylfiu5@gmail.com>
2026-05-28 21:13:57 +0200

    Modified error handler middleware so it catches ValidationErros thrown from mongoose

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import { CustomError } from "../errors/custom.error.js";
+import mongoose from "mongoose";
 
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -13,8 +14,15 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
         })
     }
 
+    if(err instanceof mongoose.Error.ValidationError) {
+        return res.status(400).json({
+            data: null,
+            message: err.message
+        })
+    }
+
     return res.status(500).json({
         data: null,
-        message: "Something went wrong!"
+        message: err.message || "Something went wrong!"
     });
 }
