ffe630714ec0d93ae72064a5370da8a031abe88b
JonZylfiu <jonzylfiu5@gmail.com>
2026-05-28 21:14:57 +0200

    Added validate id function so it validates mongoose type ids

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,5 +1,6 @@
 import type { NextFunction, Request, Response } from "express";
 import BadRequestError from "../errors/bad-request.error.js";
+import mongoose from "mongoose";
 
 export const validateHasParameter = (...args: string[]) => {
     return (req: Request, res: Response, next: NextFunction) => {
@@ -7,7 +8,9 @@ export const validateHasParameter = (...args: string[]) => {
 
         for (const arg of args) {
             if(body[arg] === undefined) {
-                throw new BadRequestError({message: `${arg} not specified`, code: 400});
+                throw new BadRequestError({
+                    message: `${arg} not specified`
+                });
             }
         }
 
@@ -21,7 +24,9 @@ export const validatePasswordLength = (req: Request, res: Response, next: NextFu
     const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/;
 
     if(!password || !regex.test(password)) {
-        throw new BadRequestError({message: "Password must contain at least 8 characters, one uppercase letter, one digit and one special character.", code: 400})
+        throw new BadRequestError({
+            message: "Password must contain at least 8 characters, one uppercase letter, one digit and one special character.",
+        })
     }
 
     next();
@@ -32,7 +37,22 @@ export const validateEmailFormat = (req: Request, res: Response, next: NextFunct
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if(!email || !regex.test(String(email).toLowerCase())) {
-        throw new BadRequestError({message: "Email is not valid!", code: 400})
+        throw new BadRequestError({
+            message: "Email is not valid!"
+        })
     }
+    
     next();
 };
+
+export const validateId = (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+
+    if (typeof id !== "string" || !mongoose.Types.ObjectId.isValid(id)) {
+        throw new BadRequestError({
+            message: "Invalid id"
+        });
+    }
+
+    next();
+}
