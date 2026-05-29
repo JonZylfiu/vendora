f5bb84a5131f01987eaf9ebe2cfe7ef9ca564a5d
JonZylfiu <jonzylfiu5@gmail.com>
2026-05-29 19:06:59 +0200

    Implemented checkEmptyArray method, which checks if an array is empty

--- src/utils/validate.util.ts
+++ src/utils/validate.util.ts
@@ -1,3 +1,4 @@
+import type { Model } from "mongoose";
 import BadRequestError from "../errors/bad-request.error.js"
 import NotAuthorizedError from "../errors/not-authorized.error.js";
 import type JwtPayload from "../types/jwt-payload.type.js";
@@ -17,4 +18,24 @@ export const checkIsAuthorized = (id: string, user: JwtPayload) => {
             message: "You're not authorized for this operation"
         });
     }
+}
+
+export const checkEmptyArray = (array: any[], arrayName: string) => {
+    if(array.length == 0) {
+        throw new BadRequestError({
+            message: `${arrayName} should not be empty!`
+        });
+    }
+}
+
+export const getEntityById = async (id: string, model: Model<any>) => {
+    const entity = await model.findById(id);
+
+    if (!entity) {
+        throw new BadRequestError({
+            message: `${model.modelName} with id ${id} does not exist!`
+        });
+    }
+
+    return entity;
 }
