887ca776e54790bf0e1b69d74e5601e82fa59db7
JonZylfiu <jonzylfiu5@gmail.com>
2026-06-03 23:16:56 +0200

    added /restore, /archive, /sold endpoints for items

--- src/controllers/item.controller.ts
+++ src/controllers/item.controller.ts
@@ -1,12 +1,11 @@
 import type { Request, Response } from "express";
 import type ItemRequestDto from "../dtos/item/item-request.dto.js";
-import { createItem, updateItem, deleteItem as deleteItemById, getItemById } from "../services/item.service.js";
+import { createItem, updateItem, deleteItem as deleteItemById, getItemById, restoreItem, soldItem, archiveItem } from "../services/item.service.js";
 import { response } from "../utils/api-response.util.js";
 
 
 
 export const create = async (req: Request<{}, {}, ItemRequestDto>, res: Response) => {
-
     const item = await createItem(req.body, req.user);
 
     return response(item, "Item is created successfully!", 201, res);
@@ -16,6 +15,21 @@ export const update = async (req: Request<{id: string}, {}, ItemRequestDto>, res
 
     const item = await updateItem(req.params.id, req.body, req.user);
     return response(item, "Item is updated successfully!", 200, res);
+}
+
+export const restore = async (req: Request<{id: string}>, res: Response) => {
+    const item = await restoreItem(req.params.id, req.user);
+    return response(item, "Item is restored successfully!", 200, res);
+}
+
+export const archive = async (req: Request<{id: string}>, res: Response) => {
+    const item = await archiveItem(req.params.id, req.user);
+    return response(item, "Item is archived successfully!", 200, res);
+}
+
+export const sold = async (req: Request<{id: string}>, res: Response) => {
+    const item = await soldItem(req.params.id, req.user);
+    return response(item, "Item is sold successfully!", 200, res);
 }
 
 
