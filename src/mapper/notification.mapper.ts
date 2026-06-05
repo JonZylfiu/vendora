26b4cfedb384f141e2164806fbdd732110fafaed
JonZylfiu <jonzylfiu5@gmail.com>
2026-06-05 15:29:23 +0200

    Added id to notification response dto

--- src/mapper/notification.mapper.ts
+++ src/mapper/notification.mapper.ts
@@ -2,9 +2,10 @@ import type INotification from "../models/interfaces/INotification.interface.js"
 
 
 export const toNotificationResponseDto = (data: INotification) => {
-    const { message, type } = data;
+    const { _id: id, message, type } = data;
 
     return {
+        id,
         message,
         type
     }
