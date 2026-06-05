462999eea34b446a6ed9e9e611d21ab0866c079a
JonZylfiu <jonzylfiu5@gmail.com>
2026-06-05 15:27:39 +0200

    Added union of undefined at createdAt field

--- src/dtos/order/order-response.dto.ts
+++ src/dtos/order/order-response.dto.ts
@@ -1,26 +1,9 @@
-export interface OrderItemResponseDto {
-    sellerFullName: string;
-    itemTitle: string;
-    quantity: number;
-    unitPrice: number;
-}
 
 export interface OrderResponseDto {
     id: string;
-
     buyerFullName: string;
-
     sellerFullName: string;
-
     itemTitle: string;
-
-    quantity: number;
-
-    unitPrice: number;
-
-    totalPrice: number;
-
     state: string;
-
-    createdAt: Date;
+    createdAt: Date | undefined;
 }
