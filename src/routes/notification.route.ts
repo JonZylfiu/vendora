1a1308c54ff7b1f2350e36e5291058d9d065a91f
JonZylfiu <jonzylfiu5@gmail.com>
2026-06-05 21:14:34 +0200

    Add ID validation to notification delete endpoint

--- src/routes/notification.route.ts
+++ src/routes/notification.route.ts
@@ -1,20 +1,20 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import { remove, getUsers } from "../controllers/notification.controller.js";
-
+import { validateId } from "../middleware/validation.middleware.js";
 
 const notificationRouter = Router();
-
 
 notificationRouter.use(authMiddleware);
 
 notificationRouter.get(
     "/",
     getUsers
-)
+);
 
 notificationRouter.delete(
     "/:id",
+    validateId,
     remove
 );
 
