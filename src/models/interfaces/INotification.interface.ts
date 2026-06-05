c67900a16552de9a0281d3070cf4bb2c89956522
JonZylfiu <jonzylfiu5@gmail.com>
2026-06-05 15:30:06 +0200

    Added _id to INotification interface

--- src/models/interfaces/INotification.interface.ts
+++ src/models/interfaces/INotification.interface.ts
@@ -2,6 +2,7 @@ import type mongoose from "mongoose";
 import type NotificationTypesEnum from "../../enums/notification-types.enum.js";
 
 export default interface INotification {
+    _id: mongoose.Types.ObjectId;
     receiver: mongoose.Types.ObjectId;
     message: string;
     type: NotificationTypesEnum;
