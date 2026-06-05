402ebed7353837990f694ab0503c39dd7a3f6d86
JonZylfiu <jonzylfiu5@gmail.com>
2026-06-05 15:31:58 +0200

    added getUserOrders at /orders endpoint

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,8 +1,9 @@
-import { Router } from "express";
+import { Router, type Request, type Response, type NextFunction } from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 import { validateHasParameter, validateId } from "../middleware/validation.middleware.js";
-import { create, getById, deleteOrder, updateState, getAll } from "../controllers/order.controller.js";
+import { create, getById, deleteOrder, updateState, getUserOrders } from "../controllers/order.controller.js";
 import { validateState } from "../middleware/order.middleware.js";
+
 
 const orderRouter = Router();
 
@@ -16,9 +17,8 @@ orderRouter.get(
 
 orderRouter.get(
     "/",
-    getAll
+    getUserOrders
 );
-
 
 orderRouter.post(
     "/",
