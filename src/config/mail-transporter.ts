45c0faa4274c4372c1f7531772573a5f5fa7ad9b
JonZylfiu <jonzylfiu5@gmail.com>
2026-06-16 23:14:16 +0200

    configured nodemailer transporter for sending emails

---
+++ src/config/mail-transporter.ts
@@ -0,0 +1,14 @@
+import nodemailer from "nodemailer";
+
+
+const transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+
+export default transporter;
