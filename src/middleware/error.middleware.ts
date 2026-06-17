16ad08a0c9da07d3f9e3430d5935efe303ce36ac
JonZylfiu <jonzylfiu5@gmail.com>
2026-06-17 18:19:36 +0200

    Added comments for describing switch statements

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -5,25 +5,31 @@ import mongoose from "mongoose";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 
+
+    // Check if error is a custom error.
     if(err instanceof CustomError) {
+        console.log(err);
+
         const { statusCode, message } = err;
         
         return res.status(statusCode).json({
-            data: null,
-            message
+            message,
+            success: false
         })
     }
 
+    // check if error comes from mongoose validation 
     if(err instanceof mongoose.Error.ValidationError) {
         console.log(err);
         return res.status(400).json({
-            data: null,
-            message: err.message
+            message: err.message,
+            success: false
         })
     }
 
+    // uncaught errors
     return res.status(500).json({
-        data: null,
-        message: err.message || "Something went wrong!"
+        message: err.message || "Something went wrong!",
+        success: false
     });
 }
