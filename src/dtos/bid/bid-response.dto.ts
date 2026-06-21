1978c6d329c26f63faf8c8ca584c91a916eca868
JonZylfiu <jonzylfiu5@gmail.com>
2026-06-21 11:55:06 +0200

    Changed bid response dto

--- src/dtos/bid/bid-response.dto.ts
+++ src/dtos/bid/bid-response.dto.ts
@@ -2,11 +2,6 @@
 export default interface BidResponseDto {
     id: string;
     bidderFullName: string;
-    bidderPhone: string;
-    item: {
-        id: string;
-        title: string;
-    };
     bidAmount: number;
     createdAt: Date;
 }   
