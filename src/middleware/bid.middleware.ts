b292e5823cd55daf315a8a38e844c8cec95daec5
JonZylfiu <jonzylfiu5@gmail.com>
2026-06-30 12:44:47 +0200

    Renamed getBidByItemsId function to getCachedBidsByItemId

--- src/middleware/bid.middleware.ts
+++ src/middleware/bid.middleware.ts
@@ -3,13 +3,12 @@ import redisClient from "../config/redis.config.js";
 import { response } from "../utils/api-response.util.js";
 
 
-export const getBidsByItemId = async (req: Request<{id: string}>, res: Response, next: NextFunction) => {
+export const getCachedBidsByItemId = async (req: Request<{id: string}>, res: Response, next: NextFunction) => {
     const bids = await redisClient.lRange(`item:${req.params.id}:bids`, 0, -1);
     
     if(!bids) {
         next();
     }
-
 
     return response(res, 200, null, bids.map(bid => JSON.parse(bid)));
 }
